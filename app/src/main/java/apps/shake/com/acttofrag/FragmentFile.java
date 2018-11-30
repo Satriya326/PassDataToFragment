c4501ddad3a59ba2ee10bc0414d55f96497cef34
NARAPATIH <narapatih@NARAPATIHs-MacBook-Pro.local>
2018-11-30 15:44:28 +0700

    change all

---
+++ app/src/main/java/apps/shake/com/acttofrag/FragmentFile.java
@@ -0,0 +1,23 @@
+package apps.shake.com.acttofrag;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFile extends Fragment {
+
+    public FragmentFile ()
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/apps/shake/com/acttofrag/MainActivity.java
+++ app/src/main/java/apps/shake/com/acttofrag/MainActivity.java
@@ -3,48 +3,42 @@ package apps.shake.com.acttofrag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-
-import apps.shake.com.acttofrag.Fragments.FragmentLogin;
-import apps.shake.com.acttofrag.Fragments.FragmentPagerAdapter;
-import apps.shake.com.acttofrag.Fragments.FragmentReceiveLogin;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentPagerAdapter mFragmentPagerAdapter;
-    private ViewPager mViewPager;
+    EditText editText1, editText2;
+    Button loginBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //to acquire fragment manager
-        mFragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager());
-        //setup pager
-        mViewPager = findViewById(R.id.viewPager);
-        setupViewPager(mViewPager);
+        FragmentManager fmanager = getSupportFragmentManager();
+        final FragmentTransaction fmTransaction = fmanager.beginTransaction();
+        final FragmentFile fragmentFile = new FragmentFile();
 
+        loginBtn = findViewById(R.id.submit);
+        editText1 = findViewById(R.id.editTextUser1);
+        editText2 = findViewById(R.id.editTextUser2);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bandle = new Bundle();
+                bandle.putString("username",editText1.getText().toString());
+                bandle.putString("psswd",editText2.getText().toString());
+                fragmentFile.setArguments(bandle);
+                fmTransaction.add(R.id.frameLayout,fragmentFile);
+                fmTransaction.commit();
+            }
+        });
 
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-    }
-
-    private void setupViewPager (ViewPager viewPager)
-    {
-        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new FragmentLogin(), "Fragment1");
-        adapter.addFragment(new FragmentReceiveLogin(), "FragmentReceiveLogin");
-
-        viewPager.setAdapter(adapter);
-    }
-
-    public void setViewPager(int fragmentNumber)
-    {
-        mViewPager.setCurrentItem(fragmentNumber);
-    }
 }
